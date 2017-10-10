06016a3cd00e68a1b8eb071b1229090d473c292c
Oksi-Oksi <maria.arabyan@gmail.com>
2017-10-10 17:59:46 +0400

    added comments

--- js/table.js
+++ js/table.js
@@ -8,9 +8,11 @@ $(document).ready(function() {
 	$('.fa-trash-o').click(function() {
 		trash_row(this);
 	});
+	//adding clones after add-row button
 	$('.add-row').click(function() {
+		//getting first .row cloning and inserting after .table-rows
 		var cloned = $('.row').first().clone().appendTo('.table-rows');
-		console.log(cloned);
+		//getting .row and finding his childs (inputs, spans) and removing cloned values
 		var cloned_input = $('.row').last().find('input').val('');
 		var cloned_span = $('.row').last().find('span').text('');
 		$('input').blur(function() {
@@ -23,40 +25,46 @@ $(document).ready(function() {
 			trash_row(this);
 		});
 	});
+	//removing row's values after clicking reset icon
 	function reset_row(self) {
 		var row = $(self).parents('.row');
 		row.find('input').val('');
 		row.find('span').text('');
 	}
+	//Deleting rows after clicking trash icon
 	function trash_row(self) {
 		var row = $(self).parents('.row').last().remove();
 	}
-
+	//Calculate inputs values when they get blur
 	function calculation(self) {
-		console.log(self);
+		//getting self(inputs) parent
 		var row = $(self).parents('.row');
-		console.log('.row:', row);
-		console.log('.row-i meji inputner@:', row.find('input'));
+		//find first and second input with their values
 		var val1 = row.find('.val-one').val();
 		var val2 = row.find('.val-two').val();
+		//finidng spans
 		var addition_span = row.find('.add-result');
 		var subtract_span = row.find('.sub-result');
 		var multiply_span = row.find('.mult-result');
 		var division_span = row.find('.div-result');
 		var modulus_span  = row.find('.mod-result');
+		//checking if 2 inputs have values then calculate else remove values
 		if (val1 && val2)  {
 			val1 = Number(val1);
 			val2 = Number(val2);
+			//calculates two inputs values
 			var adding = val1 + val2;
 			var subtracting = val1 - val2;
 			var multiplying = val1 * val2;
 			var division = val1 / val2;
 			var modulus = val1 % val2; 
+			//adding already calculated values into spans
 			addition_span.text(adding);
 			subtract_span.text(subtracting);
 			multiply_span.text(multiplying);
 			division_span.text(division);
 			modulus_span.text(modulus);
+			//else if inputs have not values delete texts of spans
 		} else {
 			row.find('span').text('');	
 		}
